1c6ed3d608a09f16aaac6286d18c40f1e48212ff
Tohdz <hhernandez70088@ufide.ac.cr>
2024-02-11 17:18:46 -0600

    New project
    Project  name practicas 3
    Folder semana 3

---
+++ practicas2/src/test/java/practicas2/demo/Practicas2ApplicationTests.java
@@ -0,0 +1,13 @@
+package practicas2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practicas2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
